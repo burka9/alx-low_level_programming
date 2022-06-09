61a64cd4c3fdf1a1161018e087f5d55e0e084c76
Biruk Ephrem <birukeph@gmail.com>
2022-06-09 21:13:30 +0300

    task done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,17 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
-	dlistint_t *temp = h;
 
-	while (temp != NULL)
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		counter++;
 	}
 
