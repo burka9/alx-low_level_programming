90b2bfb25703b6da81f4b7dc69c98398bc3918e8
Biruk Ephrem <birukeph@gmail.com>
2022-06-09 20:34:56 +0300

    task done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,18 +11,19 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t num_of_nodes = 0;
 	dlistint_t *current_node;
 
-	if (!h)
+	if (h == NULL)
 		return (num_of_nodes);
 
-	printf("%d\n", h->n);
-	num_of_nodes++;
-	current_node = h->next;
+	while (h->prev != NULL)
+		h = h->prev;
 
-	while (current_node)
+	current_node = h;
+
+	while (current_node != NULL)
 	{
 		printf("%d\n", current_node->n);
-		num_of_nodes++;
 		current_node = current_node->next;
+		num_of_nodes++;
 	}
 
 	return (num_of_nodes);
