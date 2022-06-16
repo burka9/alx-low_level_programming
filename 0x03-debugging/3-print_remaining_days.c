2a8beb3d1d00670fa535750f16b468fc51eda7cb
Biruk Ephrem <birukeph@gmail.com>
2022-06-16 12:55:10 +0300

    task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,25 +12,21 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	int days_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int remaining_days = 365;
+	int current_days = 0;
+
+	for (int i = 0; i < month - 1; i++)
 	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		current_days += days_in_month[i];
 	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+
+	current_days += days_in_month[month];
+	remaining_days -= current_days;
+
+	if (year % 4 == 0 && month == 2 && day < 29)
+		remaining_days++;
+
+	printf("Day of the year: %d", current_days);
+	printf("Remaining days: %d\n", remaining_days);
 }
