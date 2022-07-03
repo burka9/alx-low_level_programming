1910c3ba31ebfde1ec7492ff48181f5cad860767
Biruk Ephrem <birukeph@gmail.com>
2022-07-03 21:08:49 +0300

    did task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size <= 0)
 		return (NULL);
 
-	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
