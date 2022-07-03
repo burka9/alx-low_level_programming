03d53d1f4462e6033be3afbe4f167e250b162966
Biruk Ephrem <birukeph@gmail.com>
2022-07-03 11:41:18 +0300

    did task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t *array = (hash_node_t *)malloc(sizeof(hash_node_t) * size);
 
 	if (size < 1 || table == NULL || array == NULL)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = &array;
+
+	for (int i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
---
+++ 0x1A-hash_tables/main.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * main - entry point
+ * Return: 0 for success
+ */
+int main(void)
+{
+	return (0);
+}
